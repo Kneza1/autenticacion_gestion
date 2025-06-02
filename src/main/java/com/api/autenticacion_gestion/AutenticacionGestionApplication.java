6cb47cfc7e43192b7cf1e55ffd4b17b1d1af03de
benjamin castro <lilbenjalemos@gmail.com>
2025-06-02 18:17:44 -0400

    base api autenticacion_gestion

---
+++ src/main/java/com/api/autenticacion_gestion/AutenticacionGestionApplication.java
@@ -0,0 +1,13 @@
+package com.api.autenticacion_gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacionGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacionGestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/autenticacion_gestion/AutenticacionGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.autenticacion_gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
