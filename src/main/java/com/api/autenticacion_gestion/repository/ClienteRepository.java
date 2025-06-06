847f79f4844cdf4a44795ed155bc79123d79f717
benjamin castro <lilbenjalemos@gmail.com>
2025-06-06 01:46:35 -0400

    le faltan detalles minimos

---
+++ src/main/java/com/api/autenticacion_gestion/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.api.autenticacion_gestion.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.autenticacion_gestion.models.Cliente;
+
+public class ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/com/api/autenticacion_gestion/repository/VendedorRepository.java
@@ -0,0 +1,9 @@
+package com.api.autenticacion_gestion.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.autenticacion_gestion.models.Vendedor;
+
+public class VendedorRepository extends JpaRepository<Vendedor, Integer> {
+
+}
